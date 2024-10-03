8106fe18cac34830aee56f3328f844fd4435ecc0
Bjorn-23 <bjornandersson84@gmail.com>
2024-10-03 16:55:50 +0200

    fixed timeoffset from CET when creating a new form type of item in FormServices

--- UmbracoCMS-bawin23/Services/FormServices.cs
+++ UmbracoCMS-bawin23/Services/FormServices.cs
@@ -5,6 +5,14 @@ namespace UmbracoCMS_bawin23.Services;
 
 public class FormServices
 {
+    public DateTime getTime()
+    {
+        DateTime utcNow = DateTime.UtcNow;
+        TimeZoneInfo cetNow = TimeZoneInfo.FindSystemTimeZoneById("Central European Standard Time");
+        DateTime currentTime = TimeZoneInfo.ConvertTimeFromUtc(utcNow, cetNow);
+        return currentTime;
+    }
+
 
     public bool SaveFormData(CallbackRequestFormModel form, ServiceContext Services)
     {
@@ -16,7 +24,7 @@ public class FormServices
             var nodeGuid = new Guid("ef54effc-afff-4973-81f8-50aaa55c8bf9");
             var nodeId = contentService!.GetById(nodeGuid);
 
-            var callbackRequest = contentService.Create(form.Subject + " - " + form.Email + " - " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss:fff"), nodeId, "callbackRequestItem");
+            var callbackRequest = contentService.Create(form.Subject + " - " + form.Email + " - " + getTime().ToString("dd/MM/yyyy HH:mm:ss:fff"), nodeId, "callbackRequestItem");
 
             callbackRequest.SetValue("formName", form.Name);
             callbackRequest.SetValue("formEmail", form.Email);
@@ -42,7 +50,7 @@ public class FormServices
             var nodeGuid = new Guid("aed20760-f81e-4325-bcc3-d6796dd299f2");
             var nodeId = contentService!.GetById(nodeGuid);
 
-            var contactRequest = contentService.Create(form.Email + " - " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss:fff"), nodeId, "contactMessageItem");
+            var contactRequest = contentService.Create(form.Email + " - " + getTime().ToString("dd/MM/yyyy HH:mm:ss:fff"), nodeId, "contactMessageItem");
 
             contactRequest.SetValue("formName", form.Name);
             contactRequest.SetValue("formEmail", form.Email);
@@ -67,7 +75,7 @@ public class FormServices
             var nodeGuid = new Guid("548aed22-3430-4a5f-8172-619e43867214");
             var nodeId = contentService!.GetById(nodeGuid);
 
-            var callbackRequest = contentService.Create(form.Email + " - " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss:fff"), nodeId, "onlineSupportItem");
+            var callbackRequest = contentService.Create(form.Email + " - " + getTime().ToString("dd/MM/yyyy HH:mm:ss:fff"), nodeId, "onlineSupportItem");
                         
             callbackRequest.SetValue("formEmail", form.Email);            
 
