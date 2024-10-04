feadec1dd00c31f39200350ef0be2f3fc9f10ca3
Bjorn-23 <bjornandersson84@gmail.com>
2024-10-04 10:46:57 +0200

    added slider to services on index page, changed the parent section id to: our-services-index to prevent stylng from interfering.

---
+++ UmbracoCMS-bawin23/wwwroot/scripts/servicesSlider.js
@@ -0,0 +1,26 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    serviceCardSlider();
+})
+
+window.onload = () => {
+    serviceCardSlider();
+}
+
+window.addEventListener("resize", () => {
+    serviceCardSlider();
+})
+
+function serviceCardSlider() {
+    const serviceCardContainer = document.querySelector(".service-grid-container");
+    const serviceCard = document.querySelector(".service-card");
+    let serviceCardHeight = parseFloat(window.getComputedStyle(serviceCard).height);
+    let closestSection = serviceCard.closest("section");
+
+    if (window.innerWidth > 768) {
+        return
+    }
+    if (closestSection === "our-services-index") {
+        console.log(closestSection)
+        serviceCardContainer.style.height = Math.ceil(serviceCardHeight) + "px";
+    }
+}
